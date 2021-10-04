c85c283c4b3de7ad1c2dfd3231a7cb957f7cc08b
nuel-fused <eakinloose@gmail.com>
2021-10-04 05:30:18 +0100

    refactored admin-2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Switch, Route } from "react-router-dom";
-import GlobalStyles from "./GlobalStyles";
 import Home from "./pages/Home";
 import { setCurrentUser } from "./redux/User/user.action";
 import { auth, handleUserData } from "./components/firebase/utils";
@@ -12,20 +11,22 @@ import Toolbar from "./components/AdminComponents/toolbar/Toolbar";
 import ProductsPage from "./pages/Productspage";
 import ProductDetails from "./pages/ProductDetails";
 import CheckoutPage from "./pages/Checkout";
+import GlobalStyles from "./GlobalStyles";
 
-function App() {   
-
-   const dispatch = useDispatch()
+function App() {
+   const dispatch = useDispatch();
 
    useEffect(() => {
       const authListener = auth.onAuthStateChanged(async (user) => {
          if (user) {
             const userRef = await handleUserData(user);
             userRef.onSnapshot((snapshot) => {
-               dispatch(setCurrentUser({
-                  id: snapshot.id,
-                  ...snapshot.data(),
-               }));
+               dispatch(
+                  setCurrentUser({
+                     id: snapshot.id,
+                     ...snapshot.data(),
+                  })
+               );
             });
          } else {
             dispatch(setCurrentUser(user));
@@ -41,47 +42,34 @@ function App() {
    return (
       <>
          <GlobalStyles />
-         <Toolbar/>
+         <Toolbar />
          <Switch>
             <Route exact path="/" render={() => <Home />} />
-            <Route
-               exact
-               path="/dashboard"
-               render={() => <Dashboard />}
-            />
-            <Route
-               exact
-               path="/search"
-               render={() => <ProductsPage/>}
-            />
+            <Route exact path="/dashboard" render={() => <Dashboard />} />
+            <Route exact path="/search" render={() => <ProductsPage />} />
             <Route
                exact
                path="/search/:filterType"
-               render={() => <ProductsPage/>}
+               render={() => <ProductsPage />}
             />
-            <Route
-               exact
-               path="/checkout"
-               render={() => <CheckoutPage/>}
-            />
+            <Route exact path="/checkout" render={() => <CheckoutPage />} />
             <Route
                exact
                path="/product/:productID"
-               render={() => <ProductDetails/> }
+               render={() => <ProductDetails />}
             />
             <Route
                exact
                path="/admin"
-               render={() => 
-               <WithAdminAuth>
-                  <Admin />
-               </WithAdminAuth>
-            }
+               render={() => (
+                  <WithAdminAuth>
+                     <Admin />
+                  </WithAdminAuth>
+               )}
             />
          </Switch>
       </>
    );
 }
-
 
 export default App;
