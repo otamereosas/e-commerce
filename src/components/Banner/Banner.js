c52719b474a5ce1332200c6ae3591ae8b96843c8
nuel-fused <eakinloose@gmail.com>
2021-12-04 17:04:29 +0100

    progress mobile.

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -13,6 +13,26 @@ import banner1 from "../../assets/banner1.jpg";
 import banner2 from "../../assets/banner2.jpg";
 import banner3 from "../../assets/banner3.jpg";
 import banner4 from "../../assets/banner4.jpg";
+import fruits from "../../assets/fruits.jpg";
+import veg from "../../assets/veg.jpg";
+import canned from "../../assets/canned.jpg";
+import frozen from "../../assets/frozen.jpg";
+import meat from "../../assets/meat.jpg";
+import fishes from "../../assets/fishes.jpg";
+import spices from "../../assets/spices.jpg";
+import oil from "../../assets/oil.jpg";
+import snacks from "../../assets/snacks.jpg";
+// import bread from "../../assets/bread.jpg";
+import rice from "../../assets/rice.jpg";
+import cereal from "../../assets/cereal.jpg";
+import bakings from "../../assets/bakings.jpg";
+import personal from "../../assets/personal.jpg";
+// import tubers from "../../assets/tubers.jpg";
+import health from "../../assets/health.jpg";
+import tissues from "../../assets/tissues.jpg";
+// import Household from "../../assets/Household.jpg";
+// import babies from "../../assets/babies.jpg";
+import others from "../../assets/others.jpg";
 
 const Banner = () => {
    const settingsOne = {
@@ -86,34 +106,42 @@ const Banner = () => {
                <div>
                   <CategoryWrapper>
                      <Link to="/search/Fruits">
-                        <img src={banner1} alt="" />
+                        <img src={fruits} alt="" />
                      </Link>
                   </CategoryWrapper>
                </div>
                <div>
                   <CategoryWrapper>
-                     <Link to="/search/Vegetable">Vegetable</Link>
+                     <Link to="/search/Vegetable">
+                        <img src={veg} alt="" />
+                     </Link>
                   </CategoryWrapper>
                </div>
                <div>
                   <CategoryWrapper>
-                     <Link to="/search/Canned Goods">Canned Goods</Link>
+                     <Link to="/search/Canned Goods">
+                        <img src={canned} alt="" />
+                     </Link>
                   </CategoryWrapper>
                </div>
                <div>
                   <CategoryWrapper>
-                     <Link to="/search/Frozen Foods">Frozen Foods</Link>
+                     <Link to="/search/Frozen Foods">
+                        <img src={frozen} alt="" />
+                     </Link>
                   </CategoryWrapper>
                </div>
                <div>
                   <CategoryWrapper>
-                     <Link to="/search/Meat">Meat</Link>
+                     <Link to="/search/Meat">
+                        <img src={meat} alt="" />
+                     </Link>
                   </CategoryWrapper>
                </div>
                <div>
                   <CategoryWrapper>
                      <Link to="/search/Fish ands shellfish">
-                        Fish ands shellfish
+                        <img src={fishes} alt="" />
                      </Link>
                   </CategoryWrapper>
                </div>
@@ -125,75 +153,99 @@ const Banner = () => {
                <div>
                   <CategoryWrapper>
                      <Link to="/search/Condiments and Spices">
-                        Condiments and Spices
+                        <img src={spices} alt="" />
                      </Link>
                   </CategoryWrapper>
                </div>
                <div>
                   <CategoryWrapper>
-                     <Link to="/search/Sauces and Oil">Sauces and Oil</Link>
+                     <Link to="/search/Sauces and Oil">
+                        <img src={oil} alt="" />
+                     </Link>
                   </CategoryWrapper>
                </div>
                <div>
                   <CategoryWrapper>
-                     <Link to="/search/Snacks">Snacks</Link>
+                     <Link to="/search/Snacks">
+                        <img src={snacks} alt="" />
+                     </Link>
                   </CategoryWrapper>
                </div>
                <div>
                   <CategoryWrapper>
-                     <Link to="/search/Bread and Bakery">Bread and Bakery</Link>
+                     <Link to="/search/Bread and Bakery">
+                        <img src={veg} alt="" />
+                     </Link>
                   </CategoryWrapper>
                </div>
                <div>
                   <CategoryWrapper>
-                     <Link to="/search/Pasta and Rice">Pasta and Rice</Link>
+                     <Link to="/search/Pasta and Rice">
+                        <img src={rice} alt="" />
+                     </Link>
                   </CategoryWrapper>
                </div>
                <div>
                   <CategoryWrapper>
-                     <Link to="/search/Cereal">Cereal</Link>
+                     <Link to="/search/Cereal">
+                        <img src={cereal} alt="" />
+                     </Link>
                   </CategoryWrapper>
                </div>
                <div>
                   <CategoryWrapper>
-                     <Link to="/search/Baking">Baking</Link>
+                     <Link to="/search/Baking">
+                        <img src={bakings} alt="" />
+                     </Link>
                   </CategoryWrapper>
                </div>
                <div>
                   <CategoryWrapper>
-                     <Link to="/search/Personal Care">Personal Care</Link>
+                     <Link to="/search/Personal Care">
+                        <img src={personal} alt="" />
+                     </Link>
                   </CategoryWrapper>
                </div>
                <div>
                   <CategoryWrapper>
-                     <Link to="/search/Tubers">Tubers</Link>
+                     <Link to="/search/Tubers">
+                        <img src={veg} alt="" />
+                     </Link>
                   </CategoryWrapper>
                </div>
                <div>
                   <CategoryWrapper>
-                     <Link to="/search/Health Care">Health Care</Link>
+                     <Link to="/search/Health Care">
+                        <img src={health} alt="" />
+                     </Link>
                   </CategoryWrapper>
                </div>
                <div>
                   <CategoryWrapper>
-                     <Link to="/search/Paper and Wraps">Paper and Wraps</Link>
+                     <Link to="/search/Paper and Wraps">
+                        <img src={tissues} alt="" />
+                     </Link>
                   </CategoryWrapper>
                </div>
                <div>
                   <CategoryWrapper>
                      <Link to="/search/Household Supplies">
-                        Household Supplies
+                        <img src={veg} alt="" />
                      </Link>
                   </CategoryWrapper>
                </div>
                <div>
                   <CategoryWrapper>
-                     <Link to="/search/Baby Items">Baby Items</Link>
+                     <Link to="/search/Baby Items">
+                        <img src={veg} alt="" />
+                     </Link>
                   </CategoryWrapper>
                </div>
                <div>
                   <CategoryWrapper>
-                     <Link to="/search/Other Items">Other Items</Link>
+                     <Link to="/search/Other Items">
+                        <img src={others} alt="" />
+                     </Link>
                   </CategoryWrapper>
                </div>
             </Slider>
